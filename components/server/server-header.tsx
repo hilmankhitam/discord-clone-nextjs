1771b78f575a669f179eb78ea95648cb0a1120ac
hilman khitam <hilmankhitam100@gmail.com>
2023-12-11 16:11:10 +0800

    channel creation

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -69,7 +69,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="cursor-pointer px-4 py-2 text-sm ">
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel")}
+            className="cursor-pointer px-4 py-2 text-sm "
+          >
             Create Channel
             <PlusCircle className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
